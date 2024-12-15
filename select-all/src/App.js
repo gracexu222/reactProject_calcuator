62d3cd68376316e9b0d457ca8c7f5108d4bc50fa
Grace Xu <xuweijia222@gmail.com>
2024-12-15 15:36:02 -0800

    adding figures

--- select-all/src/App.js
+++ select-all/src/App.js
@@ -26,17 +26,49 @@ function SelectAllForm() {
   // check if all items are selected
   const areAllItemsSelected =
     items.length > 0 && selectedItems.length === items.length;
+
   return (
-    <div>
-      <h2>Select items</h2>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "50vh",
+        flexDirection: "column",
+        backgroundColor: "black",
+        color: "white",
+      }}
+    >
+      <h2 style={{ textAlign: "center" }}> All the selected Items:</h2>
+      <div style={{ marginBottom: "20px" }}>
+        {items.map(
+          (item, index) =>
+            !selectedItems.includes(item) && (
+              <div
+                key={index}
+                style={{
+                  margin: "10px",
+                  padding: "10px",
+                  backgroundColor: "gray",
+                  borderRadius: "5px",
+                }}
+              >
+                {item}{" "}
+              </div>
+            )
+        )}
+      </div>
+
       <label>
         <input
           type="checkbox"
           checked={areAllItemsSelected}
           onChange={handleSelectAll}
+          onClick={(e) => e.stopPropagation()}
         />
         Select All
       </label>
+
       <hr />
       {items.map((item, index) => (
         <label key={index}>
