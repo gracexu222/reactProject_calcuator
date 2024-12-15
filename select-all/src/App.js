b3daf9735b9fca144f4a954442174099953f7d41
Grace Xu <xuweijia222@gmail.com>
2024-12-14 16:01:40 -0800

    adding app.js

--- select-all/src/App.js
+++ select-all/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+function SelectAllForm() {
+  const items = ["Item1", "Item2", "Item3"];
+
+  const [selectedItems, setSelectedItems] = useState([]);
+
+  const handleSelectAll = (event) => {
+    if (event.target.checked) {
+      setSelectedItems(items);
+    } else {
+      setSelectedItems([]);
+    }
+  };
+
+  const handleItemChange = (event, item) => {
+    if (event.target.checked) {
+      setSelectedItems((prevSelected) => [...prevSelected, item]);
+    } else {
+      setSelectedItems((prevSelected) =>
+        prevSelected.filter((i) => i !== item)
+      );
+    }
+  };
+
+  // check if all items are selected
+  const areAllItemsSelected =
+    items.length > 0 && selectedItems.length === items.length;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Select items</h2>
+      <label>
+        <input
+          type="checkbox"
+          checked={areAllItemsSelected}
+          onChange={handleSelectAll}
+        />
+        Select All
+      </label>
+      <hr />
+      {items.map((item, index) => (
+        <label key={index}>
+          <input
+            type="checkbox"
+            checked={selectedItems.includes(item)}
+            onChange={(e) => handleItemChange(e, item)}
+          />
+          {item}
+        </label>
+      ))}
     </div>
   );
 }
 
-export default App;
+export default SelectAllForm;
